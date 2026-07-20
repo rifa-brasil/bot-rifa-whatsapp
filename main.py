a95ec88f44660478685ae2bcbf84586698806f64
rifa-brasil <binancebrasil2026@gmail.com>
2026-07-20 19:03:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -367,5 +367,4 @@ def webhook():
  
 if __name__ == "__main__":
     inicializar_rifa()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))) 
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
